1064f5519e8b461d0000e1e749f8dcf2f5920e7f
N-RJ <nitinjangir.2002@gmail.com>
2025-03-12 11:24:22 +0100

    prediction & trading strategy

--- TradingWebApp.py
+++ TradingWebApp.py
@@ -152,17 +152,33 @@ elif page == "Predictions":
     st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
     st.title("Prediction result:")
     
+    #getting the closing prices of last 3 trading days
     input_data=PySimFin()
     Cl_prices = input_data.get_share_prices(comp_tick,start_date,end_date)
 
-
+    #getting the predictions from the trained model
     model_obj = model_func()
     if input_data is list:
-        predictions = model_func.get_predictions(comp_tick, Cl_prices)
+        prediction = model_func.get_predictions(comp_tick, Cl_prices)
     else:
         pass
 
+    #printing the predictions
+    st.markdown(f"The predicted price of {comp_tick} for the next trading day is ${prediction}")
 
+
+    #printing the trading trading strategy
+    upper_threshold = input_data[-1] * 1.10  # +10% of d-1
+    lower_threshold = input_data[-1] * 0.95  # -5% of d-1
+
+    if prediction > upper_threshold:
+        action = "BUY"
+    elif lower_threshold <= prediction <= upper_threshold:
+        action = "HOLD"
+    else:
+        action = "SELL"
+
+    st.markdown(f"We recommend you to {action} the asset")
 
 
 elif page == "Meet the team!":
