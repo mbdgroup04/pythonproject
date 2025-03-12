ad437dc3ea25701309f5d6c9f22f7290acc66eaa
N-RJ <nitinjangir.2002@gmail.com>
2025-03-12 09:56:35 +0100

    model script

--- model_func.py
+++ model_func.py
@@ -3,7 +3,14 @@ import pandas as pd
 
 class model_func:
     def __init__(self):
-        pass
+        self.model_dict = {
+        "AAPL": pickle.load(open("models/picklemodel_AAPL.pkl", "rb")),
+        "AMZN": pickle.load(open("models/picklemodel_AMZN.pkl", "rb")),
+        "GOOG": pickle.load(open("models/picklemodel_GOOG.pkl", "rb")),
+        "MSFT": pickle.load(open("models/picklemodel_MSFT.pkl", "rb")),
+        "TSLA": pickle.load(open("models/picklemodel_TSLA.pkl", "rb"))
+        }
+    
 
-    def get_predictions(self, ticker):
-        pass
+    def get_predictions(self, ticker, cl_price:list):
+        return self.model_dict[ticker].predict(cl_price)
