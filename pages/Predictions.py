482dbba7b80bf87413f646dab1579301f6cadc84
santirhbf <santirhbf@student.ie.edu>
2025-03-13 20:37:01 +0100

    xD

--- pages/Predictions.py
+++ pages/Predictions.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-import PySimFin as psf
+from .PySimFin import PySimFin
 import datetime
+import pickle
 
 # Load Data
 @st.cache_data
