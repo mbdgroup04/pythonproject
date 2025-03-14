ed4556999f37e2dd38bef9d4693003d469caa0c9
santirhbf <santirhbf@student.ie.edu>
2025-03-14 18:19:23 +0100

    sdlkj

--- pages/functions/PySimFin.py
+++ pages/functions/PySimFin.py
@@ -83,7 +83,7 @@ class PySimFin:
   def get_dataframe(self,ticker:str):
       self.__api_key='1c8ac883-1180-4ed0-93cc-cfff0a297631'
       self.__headers = {'accept':'application/json','Authorization': f'{self.__api_key}'}
-      self.__url=f'https://backend.simfin.com/api/v3/companies/prices/verbose?ticker={ticker}&start=2024-03-05&end={(datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d')}'
+      self.__url=f'https://backend.simfin.com/api/v3/companies/prices/verbose?ticker={ticker}&start=2024-03-04&end={(datetime.today()-timedelta(days=1)).strftime('%Y-%m-%d')}'
       response=requests.get(self.__url,headers=self.__headers)
       if response.status_code == 200:
         data = response.json()
