e3d4f493a44267190efa65f24a75d0315c18c533
santirhbf <santirhbf@student.ie.edu>
2025-03-14 20:14:46 +0100

    dfasdgasdg

--- pages/Personal_Testing.py
+++ pages/Personal_Testing.py
@@ -43,6 +43,30 @@ with col2:
 st.markdown(f'<p style="font-size:40px; text-align:center; font-weight:bold; ">Personal Testing</p>', unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; "><b>TradeVision AI</b> wants to show you how well it can do even with fictional data you input in the system.</p>', unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; ">Select a company to predict the closing price for tomorrow:</p>', unsafe_allow_html=True)
+
 comp_name = st.selectbox("", ['Apple','Amazon','Google','Microsoft','Tesla'])
+if comp_name=='Apple':
+    selected_comp_name='APPLE INC'
+elif comp_name=='Amazon':
+    selected_comp_name='AMAZON COM INC'
+elif comp_name=='Google':
+    selected_comp_name='Alphabet (Google)'
+elif comp_name=='Microsoft':
+    selected_comp_name='MICROSOFT CORP'
+else:
+    selected_comp_name='Tesla'
+
+stock_df = stock_data[stock_data["Company Name"] == selected_comp_name]
+selected_ticker=stock_df.iloc[0]["Ticker"]
+
+max_fict=stock_df['Close'].max()
+mid_fict=stock_df['Close'].median()
+st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
 st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; ">First fictional closing price for {comp_name}</p>', unsafe_allow_html=True)
-fict1=st.slider("",min_value=0.0,max_value=500.0,value=150.0,step=0.1)
+fict1=st.slider("",min_value=0.0,max_value=max_fict,value=mid_fict,step=0.05)
+st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
+st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; ">Second fictional closing price for {comp_name}</p>', unsafe_allow_html=True)
+fict2=st.slider("",min_value=0.0,max_value=max_fict,value=mid_fict,step=0.05)
+st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; "><br></p>', unsafe_allow_html=True)
+st.markdown(f'<p style="font-size:20px; text-align:left; font-weight:bold; ">Third fictional closing price for {comp_name}</p>', unsafe_allow_html=True)
+fict3=st.slider("",min_value=0.0,max_value=max_fict,value=mid_fict,step=0.05)
